5fdf8b7b3e9ae96bfaa473d4c5bbecf586d8f1e0
bintai fan <bintai fan>
2016-08-09 18:44:42 +0800

    实战3更多的页面

--- code.py
+++ code.py
@@ -3,13 +3,22 @@ render = web.template.render('templates/')
 db = web.database(dbn='sqlite', db='MovieSite.db')
 
 urls = (
-    '/', 'index'
+    '/', 'index',
+	'/movie/(\d+)', 'movie',
+
 )
 
 class index:
 	def GET(self):
 		movies = db.select('movie')
 		return render.index(movies)
+
+class movie:
+    def GET(self, movie_id):
+    	movie_id = int(movie_id)
+        movie = db.select('movie', where='id=$movie_id', vars=locals())[0]
+        return render.movie(movie)
+
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
